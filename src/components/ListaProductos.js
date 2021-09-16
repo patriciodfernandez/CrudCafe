d9df610b1176449bb5f20e622d4e74f733a53d0d
pato <patricio.dfernandez@gmail.com>
2021-09-16 15:47:45 -0300

    listando productos

---
+++ src/components/ListaProductos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListaProductos = ({productos}) => {
+    return (
+        <div>
+            <h1>Lista l Productos</h1>
+        </div>
+    );
+};
+
+export default ListaProductos;
